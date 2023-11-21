878c0eb0e9b33dc0131fcf38d86452b6f8dc0e6c
shmurky <146930608+shmurky@users.noreply.github.com>
2023-11-21 21:44:25 +0100

    kocham pana Jakuba

---
+++ algorytm.py
@@ -0,0 +1,25 @@
+def wyszukaj(tab, szukana):
+    a = 0
+    b = 15
+
+    while a <= b:
+        gg = (a + b) // 2
+
+        if tab[gg] == szukana:
+            return sr
+        elif tab[gg] > szukana:
+            b = gg - 1
+        else:
+            a = gg + 1
+
+    return -1
+
+tablica = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]
+szukana_liczba = int(input("Podaj liczbę: "))
+
+pozycja = wyszukaj(tablica, szukana_liczba)
+
+if pozycja != -1:
+    print(f"Liczba {szukana_liczba} jest w {pozycja}")
+else:
+    print("nie znalazlo")
